d30a7b3232df5d7d6c4568c7d794b8c00c9760d6
Louise Lalanne <100588945+louiselalanne@users.noreply.github.com>
2022-06-28 12:33:07 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
-#---------PORT SCAN------------
+#---------PORT SCAN------------ 
+from socket import *
+import time
+startTime = time.time()
 
-import socket, sys
-
-for port in range(1,65535):
-  var = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  if var.connect_ex((sys.argv[1], port)) == 0:
-    print "Port ",port, "Open"
-    var.close()
+if __name__ == '__main__':
+   target = input('Enter the host to be scanned: ')
+   ip = gethostbyname(target)
+   print ('Starting scan on host: ', ip)
+   
+   for port in range (1, 65535):
+      s = socket(AF_INET, SOCK_STREAM)
+      
+      conn = s.connect_ex((ip, port))
+      if(conn == 0) :
+         print ('Port %d: OPEN' % (port,))
+      s.close()
+print('Time taken:', time.time() - startTime)
